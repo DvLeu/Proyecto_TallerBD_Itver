ade43cb69fbb2776cfba11559fb170853142b492
Dvleu <L21020400@veracruz.tecnm.mx>
2024-05-10 01:36:14 -0600

    Proyecto.sql Añadido aqui comenzaremos con los comandos. Signed-off-by: Dvleu <l21020400@veracruz.tecnm.mx>

---
+++ Proyecto.sql
@@ -0,0 +1,8 @@
+use fraccionamiento;
+show tables;
+describe colono_lote;
+describe clientes;
+describe catalogo_col;
+describe cargos;
+describe lote;
+-- El proyecto a realizar es un CRUD el cual haremos los comandos aqui para tener un control.
