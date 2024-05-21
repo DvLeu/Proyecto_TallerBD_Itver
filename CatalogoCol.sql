e03b4c2a40b3265942d27e46a8ec48f0fceeb49e
Dvleu <L21020400@veracruz.tecnm.mx>
2024-05-21 13:37:03 -0600

    CatalogoCol.sql Actualizado con validaciones pertinentes. Signed-off-by: Dvleu <l21020400@veracruz.tecnm.mx>

--- CatalogoCol.sql
+++ CatalogoCol.sql
@@ -1,27 +1,22 @@
--- Procedimientos almacenados en CatalogoCOl
--- Insertar en Catalogo
-use fraccionamiento;
 DELIMITER //
-CREATE PROCEDURE InsertCatalogoCol(IN _CA_CLAVE char(3), IN _CA_TIPO smallint, IN _CA_DESCRIPCION char(30), IN _CA_IMPORTE double, IN _CON_CLAVE smallint)
+
+CREATE PROCEDURE InsertCatalogoCol(
+    IN CA_CLAVE CHAR(3),
+    IN CA_TIPO SMALLINT,
+    IN CA_DESCRIPCION CHAR(30),
+    IN CA_IMPORTE DOUBLE,
+    IN CON_CLAVE SMALLINT)
 BEGIN
-  INSERT INTO catalogo_col (CA_CLAVE, CA_TIPO, CA_DESCRIPCION, CA_IMPORTE, CON_CLAVE)
-  VALUES (_CA_CLAVE, _CA_TIPO, _CA_DESCRIPCION, _CA_IMPORTE, _CON_CLAVE);
+    -- Validación de la clave primaria: no debe ser vacía ni nula
+    IF CA_CLAVE IS NULL OR CA_CLAVE = '' THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error: La clave CA_CLAVE no puede estar vacía.';
+    END IF;
+
+    -- Inserción de los datos
+    INSERT INTO catalogo_col (CA_CLAVE, CA_TIPO, CA_DESCRIPCION, CA_IMPORTE, CON_CLAVE)
+    VALUES (CA_CLAVE, CA_TIPO, CA_DESCRIPCION, CA_IMPORTE, CON_CLAVE);
 END //
+
 DELIMITER ;
--- Actualizar en Catalogo
-DELIMITER //
-CREATE PROCEDURE UpdateCatalogoCol(IN _CA_CLAVE char(3), IN _CA_TIPO smallint, IN _CA_DESCRIPCION char(30), IN _CA_IMPORTE double, IN _CON_CLAVE smallint)
-BEGIN
-  UPDATE catalogo_col
-  SET CA_TIPO = _CA_TIPO, CA_DESCRIPCION = _CA_DESCRIPCION, CA_IMPORTE = _CA_IMPORTE, CON_CLAVE = _CON_CLAVE
-  WHERE CA_CLAVE = _CA_CLAVE;
-END //
-DELIMITER ;
--- Borrar en Catalogo
-DELIMITER //
-CREATE PROCEDURE DeleteCatalogoCol(IN _CA_CLAVE char(3))
-BEGIN
-  DELETE FROM catalogo_col
-  WHERE CA_CLAVE = _CA_CLAVE;
-END //
-DELIMITER ;
+
+
--- Proyecto.sql
+++ Proyecto.sql
@@ -8,4 +8,28 @@ describe lote;
 
 -- El proyecto a realizar es un CRUD el cual haremos los comandos aqui para tener un control.\
 
+drop procedure IF EXISTS InsertCatalogoCol;
 
+
+DELIMITER //
+
+CREATE PROCEDURE InsertCatalogoCol(
+    IN CA_CLAVE CHAR(3),
+    IN CA_TIPO SMALLINT,
+    IN CA_DESCRIPCION CHAR(30),
+    IN CA_IMPORTE DOUBLE,
+    IN CON_CLAVE SMALLINT)
+BEGIN
+    -- Validación de la clave primaria: no debe ser vacía ni nula
+    IF CA_CLAVE IS NULL OR CA_CLAVE = '' THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error: La clave CA_CLAVE no puede estar vacía.';
+    END IF;
+
+    -- Inserción de los datos
+    INSERT INTO catalogo_col (CA_CLAVE, CA_TIPO, CA_DESCRIPCION, CA_IMPORTE, CON_CLAVE)
+    VALUES (CA_CLAVE, CA_TIPO, CA_DESCRIPCION, CA_IMPORTE, CON_CLAVE);
+END //
+
+DELIMITER ;
+
+CALL InsertCatalogoCol('ABC', 1, 'Descripción', 100.50, 123);
